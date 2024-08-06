01b5092223b61edd063ff193fc9a3e3210410806
Venkata Suresh <shashankyarava1234@gmail.com>
2024-08-06 18:25:11 +0530

    Added code to segment characters from captcha

--- extract_single_captcha_image.py
+++ extract_single_captcha_image.py
@@ -6,6 +6,8 @@ CAPTCHA_IMAGE_FOLDER = "./captcha_images"
 OUTPUT_FOLDER = "./generated_single_images"
 
 
+count = 0 # using for giving image a unique name
+
 def pad_array_to_size(array, target_size):
     # Get the current shape of the array
     current_shape = array.shape
@@ -23,47 +25,93 @@ def pad_array_to_size(array, target_size):
     return padded_array
 
 
-def extract_parts_from_contours(contours, name):
-    letter_image_regions = []
-    for contour in contours:
-        # Get the rectangle that contains the contour
-        (x, y, w, h) = cv2.boundingRect(contour)
+def find_large_zero_groups(data, threshold=1):
+    data = np.array(data)
 
-        # Compare the width and height of the contour to detect letters that
-        # are conjoined into one chunk
-        if w / h > 1.25:
-            # This contour is too wide to be a single letter!
-            # Split it in half into two letter regions!
-            half_width = int(w / 2)
-            letter_image_regions.append((x, y, half_width, h))
-            letter_image_regions.append((x + half_width, y, half_width, h))
+    zero_groups = []
+    count = 0
+    start_index = -1
+
+    for i in range(len(data)):
+        if data[i] == 0:
+            if count == 0:
+                start_index = i
+            count += 1
         else:
-            # This is a normal letter by itself
-            letter_image_regions.append((x, y, w, h))
-    letter_image_regions = sorted(letter_image_regions, key=lambda x: x[0])
+            if count > threshold:
+                zero_groups.append((start_index, count))
+            count = 0
 
-    # if the contours are more than the characters than we have an error in segmenting the characters
-    if len(letter_image_regions) != 4:
-        character_path = os.path.join(OUTPUT_FOLDER, name)
-        cv2.imwrite(character_path ,binary_image )
+    if count > threshold:
+        zero_groups.append((start_index, count))
+
+    return zero_groups
+
+def split_images(grps,average_character_width,name):
+    global count
+    index = 0
+    for start,end in grps:
+        left = start
+        upper = 0
+        lower = height
+        right = max(end, start+average_character_width)
+        character = binary_image[upper : lower , left:right+1]
+        character = pad_array_to_size(character, (50, 50))
+        try:
+            path = os.path.join(OUTPUT_FOLDER,name.split(".")[0][index])
+            cv2.imwrite(path+f"_{count}_"+".png",character)
+            index+=1
+            count+=1
+        except:
+            print(name)
+
+
+
+def segment_characters_from_binary_image(binary_image, name):
+    binImage_cpy = binary_image.copy()
+    binImage_cpy[binImage_cpy == 0] = 1
+    binImage_cpy[binImage_cpy == 255] = 0
+    vertical_projection = np.sum(binImage_cpy, axis=0)
+    zero_grps = find_large_zero_groups(vertical_projection)
+
+    non_zero_grps = []
+
+    # finding non zero groups
+
+    for i in range(len(zero_grps) - 1):
+        start = zero_grps[i][0] + zero_grps[i][1]
+        end = zero_grps[i + 1][0]
+        non_zero_grps.append((start, end - 1))
+    average_character_width = 0
+
+    for x, y in non_zero_grps:
+        average_character_width += y - x + 1
+
+    average_character_width = (average_character_width // len(name.split(".")[0]))
+
+    if len(non_zero_grps) == len(name.split(".")[0]):
+        split_images(non_zero_grps,average_character_width,name)
     else:
-        for i in range(len(letter_image_regions)):
-            x, y, w, h = letter_image_regions[i]
-            copy = binary_image[y - 2:y + h + 2, x - 2:x + w + 2]
-            copy = 255 - copy
-            copy = pad_array_to_size(copy, (35, 35))
-            character_path = os.path.join(OUTPUT_FOLDER, name[i])
-            cv2.imwrite(character_path + ".png", copy)
+        grps = []
+        threshold = average_character_width // 2  # to check if the width goes more than the threshold
+        for start, end in non_zero_grps:
+            if abs(average_character_width - (end - start)) > threshold:
+                if end <= start + average_character_width + 2:
+                    grps.append((start, start + average_character_width + 2))
+                else:
+                    grps.append((start, start + average_character_width + 2))
+                    grps.append((start + average_character_width, end))
+            else:
+                grps.append((start, end))
+        split_images(grps,average_character_width,name)
 
 
 for image in os.listdir(CAPTCHA_IMAGE_FOLDER):
     path = os.path.join(CAPTCHA_IMAGE_FOLDER, image)
     image_path = cv2.imread(path)  # reading image
-
+    height = image_path.shape[0]
     gray = cv2.cvtColor(image_path, cv2.COLOR_BGR2GRAY)  # converting BGR format to gray
 
     _, binary_image = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-
-    contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # finding contours
-
-    extract_parts_from_contours(contours, image)
+    binary_image = 255 - binary_image
+    segment_characters_from_binary_image(binary_image, image)
